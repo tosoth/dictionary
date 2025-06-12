52eb131c40c0ccf075b4e36796841a04ef459ed2
tosoth <tosoth@gmail.com>
2025-06-12 23:03:29 +0800

    Clarify the UI, show with colors

--- dict.py
+++ dict.py
@@ -1,37 +1,69 @@
 import tkinter as tk
 from tkhtmlview import HTMLLabel
+import webview
 
 import datetime
 import os
 import sys
+import re
 
 from pystardict import Dictionary
 
-# In my case, my stardict file is in C:\\Users\\2346318\\coding folder
-# And there are 4 files that need to be prepared
-# Collins5.dict.dz
-# Collins5.idx
-# Collins5.ifo
-# Collins5.syn
-
-dicts_dir = 'C:\\Users\\xxxxxx\\coding'
+dicts_dir = 'C:\\Users\\2346318\\coding'
 dict1 = Dictionary(os.path.join(dicts_dir, 'Collins5'))
+
+# List to store the search history
+search_history = []
+
+def fix_bold_space(html_string):
+    """
+    Replaces "</b> <b>" with "</b>&nbsp;<b>" in an HTML string.
+
+    Args:
+        html_string (str): The HTML string to modify.
+
+    Returns:
+        str: The modified HTML string.
+    """
+    return re.sub(r"</b> <b>", r" ", html_string)
+
+def modernize_font_tags(html_snippet):
+    """Replaces <FONT COLOR="..."> with <span style="color: ...">."""
+    pattern = r'<FONT COLOR="([^"]*)">(.*?)</FONT>'
+    replacement = r'<span style="color: \1">\2</span>'
+    return re.sub(pattern, replacement, html_snippet, flags=re.IGNORECASE)
 
 # Define the function to display HTML content
 def show_html_content(event=None):
     global the_word, html_label
     the_word = input_box.get()
 
-    html_content = dict1.dict[the_word]
+    html_content = modernize_font_tags(dict1.dict[the_word])
+
+    html_content = fix_bold_space(html_content)
+
+    #html_content = dict1.dict[the_word]
+    #print(html_content)
 
     if 'html_label' in globals():
         html_label.pack_forget()  # Remove the old HTMLLabel widget
 
     html_label = HTMLLabel(root, html=html_content)
     html_label.pack()
+
+    window = webview.create_window('HTML Viewer', html=html_content)
+    #webview.start()
     
     input_box.select_range(0, tk.END)
     input_box.icursor(tk.END)
+
+    word = the_word
+    # Append the searched word to the search history
+    if word not in search_history:
+        search_history.append(word)
+        # Save the updated search history to the file
+        with open('history-word-list.txt', 'a') as file:
+            file.write(word + '\n')
 
 def exit_app(event=None):
     root.destroy()
@@ -39,6 +71,14 @@ def exit_app(event=None):
 # Create the main window
 root = tk.Tk()
 root.title("Collins5 stardict app")
+
+
+
+import ctypes
+try:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)  # Enable DPI awareness
+except:
+    pass
 
 input_box = tk.Entry(root, width=50)
 input_box.pack(pady=10)
@@ -53,7 +93,6 @@ root.bind('<Escape>', exit_app)
 
 # Set focus to the input box when the app starts
 input_box.focus_set()
-
 
 # Run the application
 root.mainloop()
